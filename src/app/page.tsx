ffe24dffe72589eddf726b60152030cc170e98b7
Ayush Goel <117983314+Ayush-Goel20@users.noreply.github.com>
2025-08-26 20:30:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import Navbar from "./components/NavBar";
 import Explore from "./components/ExploreSection";
 import Footer from "./components/Footer";
+import Testimonial from "@/components/Testimonial";
+
 export default function Home() {
   return <div>
         <Navbar />
